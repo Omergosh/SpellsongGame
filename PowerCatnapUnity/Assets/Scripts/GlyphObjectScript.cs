90175dbc2f8d46ae4ce1b9a23fe810703fafa8c4
Omer Ahmed <omera407@gmail.com>
2021-08-15 18:34:45 -0600

    Created template prefabs for CardDisplays (cards shown on screen) and Glyphs

---
+++ PowerCatnapUnity/Assets/Scripts/GlyphObjectScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlyphObjectScript : MonoBehaviour
+{
+    public SpriteRenderer glyphSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
